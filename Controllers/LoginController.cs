f3a44ceeeae69d613d788512dd41e4a19db6eba0
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-06-25 18:21:40 +0300

    register ve login page eklendir

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore5_blogsite.Controllers
+{
+	public class LoginController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ ViewComponents/Blog/WriterLastBlog.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore5_blogsite.ViewComponents.Blog
+{
+	public class WriterLastBlog: ViewComponent
+	{
+		BlogManager bm = new BlogManager(new EfBlogRepository());
+		public IViewComponentResult Invoke()
+		{
+			var values = bm.GetBlogListByWriter(1);
+			return View(values);
+		}
+	}
+}
